b8ddf5d3202a402379eafe0d19ebd15d988c9e1a
Murkorus <95182180+Murkorus@users.noreply.github.com>
2025-08-05 12:26:18 +0200

    Update logging behaviour and fix fullscreen

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-echo "$(date): Started startup.sh - Starting python script" >> /home/pi/xprofile_debug.log
+echo "$(date): Started startup.sh - Starting python script" >> /home/pi/Desktop/debug.log
 
 # Firstly, connect to wifi. This should be handled automatically by the Pi if wifi has been configured correctly previously.
 
@@ -6,13 +6,13 @@ echo "$(date): Started startup.sh - Starting python script" >> /home/pi/xprofile
 source /home/pi/PyVenv/bin/activate
 python /home/pi/Scanner/backgroundScript.py &
 
-echo "$(date): startup.sh passed starting python script - pinging google.com" >> /home/pi/xprofile_debug.log
+echo "$(date): startup.sh passed starting python script - pinging google.com" >> /home/pi/Desktop/debug.log
 
 # Wait a bit to make sure everything has started
 until ping -c1 google.com &>/dev/null; do sleep 1; done
 sleep 5
 
-echo "$(date): startup.sh passed pinging google.com - Launching chromium" >> /home/pi/xprofile_debug.log
+echo "$(date): startup.sh passed pinging google.com - Launching chromium" >> /home/pi/Desktop/debug.log
 
 # Launch the website
 chromium-browser \
@@ -21,10 +21,10 @@ chromium-browser \
   --disable-software-rasterizer \
   --disable-extensions \
   --disable-sync \
-  --start-fullscreen \
+  --start \
   --app=https://sorter.noelg.dk/scanner &
 
-echo "$(date): startup.sh passed starting chromium - Simulating keypresses" >> /home/pi/xprofile_debug.log
+echo "$(date): startup.sh passed starting chromium - Simulating keypresses" >> /home/pi/Desktop/debug.log
 
 # Wait a bit to make sure the website has booted up
 sleep 8
@@ -40,4 +40,4 @@ sleep 2
 # Make the popup window fullscreen
 xdotool key F11
 
-echo "$(date): startup.sh finished executing - Startup completed, quitting." >> /home/pi/xprofile_debug.log
+echo "$(date): startup.sh finished executing - Startup completed, quitting." >> /home/pi/Desktop/debug.log
--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
-echo "$(date): Started update.sh - Pulling from GitHub" >> /home/pi/xprofile_debug.log
+echo "$(date): Started update.sh - Pulling from GitHub" >> /home/pi/Desktop/debug.log
 # Fetch and pull from GitHub to get newest file changes.
 cd /home/pi/Scanner
 git fetch
 git pull
 
-echo "$(date): update.sh passed GitHub pull, starting startup.sh" >> /home/pi/xprofile_debug.log
+echo "$(date): update.sh passed GitHub pull, starting startup.sh" >> /home/pi/Desktop/debug.log
 # Run the startup script
-bash /home/pi/Scanner/startup.sh > /home/pi/Desktop/startup_log.txt 2>&1
+bash /home/pi/Scanner/startup.sh > /home/pi/Desktop/debug.log 2>&1
 
