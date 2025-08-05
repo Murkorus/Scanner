9e67858cee019bb5fa7debf2cb3e148ccbaa52f2
Murkorus <95182180+Murkorus@users.noreply.github.com>
2025-08-05 12:12:13 +0200

    Added logging for debugging

--- startup.sh
+++ startup.sh
@@ -1,12 +1,18 @@
+echo "$(date): Started startup.sh - Starting python script" >> /home/pi/xprofile_debug.log
+
 # Firstly, connect to wifi. This should be handled automatically by the Pi if wifi has been configured correctly previously.
 
 # Start python script, first need to activate virtual environment
 source /home/pi/PyVenv/bin/activate
 python /home/pi/Scanner/backgroundScript.py &
 
+echo "$(date): startup.sh passed starting python script - pinging google.com" >> /home/pi/xprofile_debug.log
+
 # Wait a bit to make sure everything has started
 until ping -c1 google.com &>/dev/null; do sleep 1; done
 sleep 5
+
+echo "$(date): startup.sh passed pinging google.com - Launching chromium" >> /home/pi/xprofile_debug.log
 
 # Launch the website
 chromium-browser \
@@ -17,6 +23,8 @@ chromium-browser \
   --disable-sync \
   --start-fullscreen \
   --app=https://sorter.noelg.dk/scanner &
+
+echo "$(date): startup.sh passed starting chromium - Simulating keypresses" >> /home/pi/xprofile_debug.log
 
 # Wait a bit to make sure the website has booted up
 sleep 8
@@ -31,3 +39,5 @@ sleep 2
 
 # Make the popup window fullscreen
 xdotool key F11
+
+echo "$(date): startup.sh finished executing - Startup completed, quitting." >> /home/pi/xprofile_debug.log
--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
+echo "$(date): Started update.sh - Pulling from GitHub" >> /home/pi/xprofile_debug.log
 # Fetch and pull from GitHub to get newest file changes.
 cd /home/pi/Scanner
 git fetch
 git pull
 
+echo "$(date): update.sh passed GitHub pull, starting startup.sh" >> /home/pi/xprofile_debug.log
 # Run the startup script
 bash /home/pi/Scanner/startup.sh > /home/pi/Desktop/startup_log.txt 2>&1
 
