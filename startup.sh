68b49ebcc5c77a3d430b68453a5d6175397a5259
Murkorus <95182180+Murkorus@users.noreply.github.com>
2025-08-05 15:04:02 +0200

    Try nohup

--- startup.sh
+++ startup.sh
@@ -15,14 +15,15 @@ sleep 5
 echo "$(date): startup.sh passed pinging google.com - Launching chromium" >> /home/pi/Desktop/debug.log
 
 # Launch the website
-chromium-browser \
+nohup chromium-browser \
   --no-sandbox \
   --kiosk \
   --app=https://sorter.noelg.dk/scanner \
   --disable-features=TranslateUI \
   --disable-gpu \
   --disable-extensions \
-  --start-maximized &
+  --start-maximized \
+  > /dev/null 2>&1 & disown
 
 echo "$(date): startup.sh passed starting chromium - Simulating keypresses" >> /home/pi/Desktop/debug.log
 
