b0ec8f2e6e7275d7448f043f559c41131ae04441
Murkorus <95182180+Murkorus@users.noreply.github.com>
2025-08-05 13:03:53 +0200

    Abusing chromium

--- startup.sh
+++ startup.sh
@@ -38,5 +38,21 @@ sleep 10
 
 # Make the popup window fullscreen
 xdotool key F11
+sleep 1
+xdotool key F11
+sleep 1
+xdotool key F11
+sleep 1
+xdotool key F11
+sleep 1
+xdotool key F11
+sleep 1
+xdotool key F11
+sleep 1
+xdotool key F11
+sleep 1
+xdotool key F11
+sleep 1
+xdotool key F11
 
 echo "$(date): startup.sh finished executing - Startup completed, quitting." >> /home/pi/Desktop/debug.log
