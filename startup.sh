a093157be6dc66d1fe34bb288fe07a36ae0dad65
Murkorus <95182180+Murkorus@users.noreply.github.com>
2025-08-05 12:46:02 +0200

    Update confirmation insurance

--- startup.sh
+++ startup.sh
@@ -38,6 +38,7 @@ sleep 1
 
 # Make the popup window fullscreen
 xdotool key F11
+echo "$(date): startup.sh got past first F11 press - pressing again in a bit" >> /home/pi/Desktop/debug.log
 sleep 15
 xdotool key F11
 
